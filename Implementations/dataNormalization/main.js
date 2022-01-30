2b730695d2c0c8c438d4c3c0b42ce0726442a4c7
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-01-30 11:23:29 +0200

    update the `forEach` with test element

--- Implementations/dataNormalization/main.js
+++ Implementations/dataNormalization/main.js
@@ -1,6 +1,30 @@
+let app_name = "Normalization app running...";
+
 clog = (object) => {
   console.log(object);
 };
 
-app_name = "Normalization app running...";
+/* APP PRINT */
 clog(app_name);
+/* ********* */
+
+function randomArray(size, seed) {
+  // create the array with a fixed size
+  let retArray = new Array(size);
+  clog(retArray.length);
+  for (let index = 0; index < retArray.length; index++) {
+    retArray[index] = seed;
+  }
+
+  // change the element of the array to a random one
+  retArray.forEach((val) => clog("->" + val));
+
+  return retArray;
+}
+
+arr = randomArray(5, 2);
+clog(arr);
+
+// let arr = [1, 2, 3, 4];
+// arr.forEach((val, index) => (arr[index] = val * val));
+// console.log(arr);
---
+++ Implementations/dataNormalization/notes.md
@@ -0,0 +1,7 @@
+# Data normalization
+
+1. create a matrix $N\times M$
+   1. implement function that returns a random array (size `M`)
+   2. repeat the process N times, add those arrays to a _main-array_ (which will become the actual matrix).
+2. numerical data
+   1. have a random number generator (function) -> arbitrary size: `random-array`
